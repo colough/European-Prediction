407b8b119ec4bbf2d6adb04ed4f71e28d7937bd6
colough <31996731+colough@users.noreply.github.com>
2018-02-10 16:41:54 -0500

    Fixed bugs aggregate folder added

--- Raw File Aggregator.r
+++ Raw File Aggregator.r
@@ -7,7 +7,7 @@
 # Course not, likewise remember your packages soldier
 
 require(data.table)
-
+require(lubridate)
 ################################################################################
 #-=----------------------------Parameter Setting-------------------------------#
 ################################################################################
@@ -18,20 +18,23 @@ Country <- c("England", "France", "Spain", "Italy", "Germany")
 ################################################################################
 #------------------------------Data Aggregation--------------------------------#
 ################################################################################
-# Spoiler Alert: We'll need this later
-Agg_Data <- data.frame()
+
 # ok loop through and glue everything together
 for (i in 1:length(Country)){
     # Where are the files located?
-    file_location <- paste0("C:/Users/coloughlin/OneDrive/SONY_16M1/",
+    file_location <- paste0("C:/Users/ciana/OneDrive/SONY_16M1/",
     "Football Predictions/",Country[i],"/Raw Data")
     setwd(file_location)
     file_list <- list.files()
+    file_list <- file_list[file_list != "Aggregated"]
+    # Spoiler Alert: We'll need this later
+    Agg_Data <- data.frame()
     for (j in 1:length(file_list)){
         df <- read.csv(file_list[j], header=TRUE)
         Agg_Data <- rbindlist(list(Agg_Data,df), use.names=TRUE, fill=TRUE)
     }
-    write.csv(Agg_Data, paste0("Aggregated_Raw_Data_",Country[i],".csv"),
-    row.names=F)
+    Agg_Data$Date <- lubridate::dmy(Agg_Data$Date)
+    write.csv(Agg_Data, paste0("Aggregated/Aggregated_Raw_Data_",
+    Country[i],".csv"),row.names=F)
 }
 # tidy
